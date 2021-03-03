d79da6c5ef32b6a6e46a55d0c772fdf13175045a
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-03-03 11:10:34 +0300

    Changing API prefix

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,6 @@
 const apiPort = process.env.API_PORT ? ':' + process.env.API_PORT : '',
   sitePort = process.env.NODE_ENV == ('development' as string) ? `:${process.env.SITE_PORT}` : '',
-  rootApiAppendix = 'saviour';
+  rootApiAppendix = 'money';
 
 export const apiHost = `${process.env.API_SCHEME}://${process.env.API_HOST}${apiPort}/${rootApiAppendix}`,
   wsHost = `${process.env.API_WS_SCHEME}://${process.env.API_HOST}${apiPort}/${rootApiAppendix}`,
