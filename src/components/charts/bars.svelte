7c6a3dbec505d0a75d5838c3a265645dfd11a9b3
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-05-24 19:08:55 +0300

    Fixing chart bugs at last

--- src/components/charts/bars.svelte
+++ src/components/charts/bars.svelte
@@ -29,20 +29,32 @@
 
 <script>
   import { chartjs } from '$utils/actions/chartjs';
+  import { tick } from 'svelte';
 
   export let dataset: { backgroundColor: string[]; borderColor: string[]; data: number[] },
     labels: string[],
     type = 'horizontalBar',
     height: number | undefined = undefined;
 
-  $: if (!height) {
-    if (dataset.data?.length < 5) height = (dataset.data.length || 0) * 50;
-    else height = (dataset.data?.length || 0) * 30;
+  let innerHeight = height,
+    show = true;
+  $: if (typeof height == 'undefined' && dataset.data) {
+    show = false;
+
+    const length = dataset.data.length || 0;
+    innerHeight = length * (length < 5 ? 50 : 30);
+    console.log({ height, innerHeight });
+
+    // I just couldn't manage to force this piece of shit to maintain a certain height.
+    // It was always rewritten by chart.js, this is why we cheat like this.
+    tick().then(() => (show = true));
   }
 </script>
 
 <div class="py-4">
-  {#key height}<canvas
-      {height}
-      use:chartjs={{ ...chartSettings, type, data: { labels, datasets: [dataset] } }} />{/key}
+  {#if show}
+    <canvas
+      height={innerHeight}
+      use:chartjs={{ ...chartSettings, type, data: { labels, datasets: [dataset] } }} />
+  {/if}
 </div>
