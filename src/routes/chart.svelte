88712eeff9ec01bb33e0ad09e2636b7e3cc04715
Daniil Kozlov <d.kzlv.ru@gmail.com>
2021-05-31 12:19:37 +0300

    Start small, lol

---
+++ src/routes/chart.svelte
@@ -0,0 +1,9 @@
+<script>
+  import UnAuthLayout from '$components/nav/unAuthLayout.svelte';
+
+  import Line from '$components/chart/line.svelte';
+</script>
+
+<UnAuthLayout>
+  <Line />
+</UnAuthLayout>
